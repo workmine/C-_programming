0214547748e6d0b9e2e93b40c42519dfe9ef6dd0
Suman Nayak <144478630+workmine@users.noreply.github.com>
2025-11-19 00:07:40 +0530

    Add files via upload

---
+++ ASS3_pri2.c
@@ -0,0 +1,13 @@
+// Area of Rectangle
+#include<stdio.h>
+int main ()
+{
+	int length, breadth;
+	printf("Enter the length and breadth is: ");
+		scanf("%d %d", &length, &breadth);
+		int area;
+		area = length * breadth;
+		printf("The area is :%d", area);
+		return 0;
+}
+
